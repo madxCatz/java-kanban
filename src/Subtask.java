f9501015781c274cd4be996717bb254454bef3f0
Nikita Sara <madxcatz@yandex.ru>
2024-07-25 00:23:38 +0300

    Выполнен code-style: добавлен пробел в конструктор Subtask

--- src/Subtask.java
+++ src/Subtask.java
@@ -2,7 +2,7 @@ public class Subtask extends Task {
 
     private final int epicId;
 
-    public Subtask (String taskName, String description, TaskStatus status, int epicId) {
+    public Subtask(String taskName, String description, TaskStatus status, int epicId) {
         super(taskName, description, status);
         this.epicId = epicId;
     }
